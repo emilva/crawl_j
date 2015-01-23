4f6c231959b7abe5a349da6a044263c2bccd91aa
reiz <robert.reiz.81@gmail.com>
2015-01-23 20:39:20 +0100

    Add Adobe Mojo

---
+++ versioneye_maven_crawler/src/main/java/versioneye/mojo/AdobeMojo.java
@@ -0,0 +1,27 @@
+package versioneye.mojo;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+
+@Mojo( name = "adobe", defaultPhase = LifecyclePhase.PROCESS_SOURCES )
+public class AdobeMojo extends HtmlMojo {
+
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        try{
+            super.execute();
+
+            username = null;
+            password = null;
+
+            setRepository("adobe");
+
+            crawl();
+        } catch( Exception exception ){
+            getLog().error(exception);
+            throw new MojoExecutionException("Oh no! Something went wrong. Get in touch with the VersionEye guys and give them feedback.", exception);
+        }
+    }
+
+}
