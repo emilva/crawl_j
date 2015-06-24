0b75e69d941aedebf8428c1cbc65309fd782c73a
reiz <robert.reiz.81@gmail.com>
2015-06-24 11:27:29 +0200

    Refactor Group1Mojo

--- versioneye_maven_crawler/src/main/java/versioneye/mojo/Group1Mojo.java
+++ versioneye_maven_crawler/src/main/java/versioneye/mojo/Group1Mojo.java
@@ -5,7 +5,9 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 import org.eclipse.aether.artifact.Artifact;
+import org.eclipse.aether.resolution.ArtifactResult;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import versioneye.crawler.CrawlerMavenDefaultHtml;
@@ -13,30 +15,42 @@ import versioneye.crawler.ICrawl;
 import versioneye.domain.Repository;
 import versioneye.service.ProductService;
 
+import java.io.File;
+
 /**
  * Crawles all repositories in group1.
  */
 @Mojo( name = "group1", defaultPhase = LifecyclePhase.PROCESS_SOURCES )
-public class Group1Mojo extends SuperMojo {
+public class Group1Mojo extends AetherMojo {
+
+    @Parameter( defaultValue = "lamina", property = "groupIdC")
+    protected String groupIdC;
+
+    @Parameter( defaultValue = "lamina", property = "artifactIdC")
+    protected String artifactIdC;
+
+    @Parameter( defaultValue = "0.5.0", property = "${versionC}")
+    protected String versionC;
 
     protected ProductService productService;
     private ICrawl crawlerMavenDefaultHtml;
 
     public void execute() throws MojoExecutionException, MojoFailureException {
         try{
-            ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+            super.execute();
+
             productService = (ProductService) context.getBean("productService");
             crawlerMavenDefaultHtml = (CrawlerMavenDefaultHtml) context.getBean("crawlerMavenDefaultHtml");
             super.execute();
 
             setRepository("CloJars", "http://clojars.org/repo");
 
-            Artifact artifactInfo = getArtifact("lamina:lamina:0.5.0");
+            getLog().info("run for " + groupIdC + ":" + artifactIdC + ":pom:" + versionC);
+            Artifact artifactInfo = getArtifact( groupIdC + ":" + artifactIdC + ":pom:" + versionC);
 
-            resolveArtifact(artifactInfo);
+            ArtifactResult artifactResult = resolveArtifact(artifactInfo);
             resolveDependencies(artifactInfo);
-            parseArtifact(artifactInfo, null);
-
+            parseArtifact(artifactResult.getArtifact(), null);
         } catch( Exception exception ){
             getLog().error(exception);
             throw new MojoExecutionException("Oh no! Something went wrong. Get in touch with the VersionEye guys and give them feedback.", exception);
