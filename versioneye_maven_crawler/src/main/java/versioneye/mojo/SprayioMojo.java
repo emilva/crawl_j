f45bab2e37fd8fa3acd3d27d260a5f52c3126582
reiz <robert.reiz.81@gmail.com>
2015-06-25 21:55:42 +0200

    Add spryaio mojo

---
+++ versioneye_maven_crawler/src/main/java/versioneye/mojo/SprayioMojo.java
@@ -0,0 +1,27 @@
+package versioneye.mojo;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+
+@Mojo( name = "jcenter", defaultPhase = LifecyclePhase.PROCESS_SOURCES )
+public class SprayioMojo extends HtmlMojo {
+
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        try{
+            super.execute();
+
+            username = null;
+            password = null;
+
+            setRepository("sprayio");
+
+            crawl();
+        } catch( Exception exception ){
+            getLog().error(exception);
+            throw new MojoExecutionException("Oh no! Something went wrong. Get in touch with the VersionEye guys and give them feedback.", exception);
+        }
+    }
+
+}
