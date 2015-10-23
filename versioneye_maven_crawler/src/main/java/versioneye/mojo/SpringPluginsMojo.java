fc46a42ff623ba3cc6d0a4fa69cd0d530692186e
reiz <robert.reiz.81@gmail.com>
2015-10-23 17:34:25 +0200

    Little BugFix.

--- versioneye_maven_crawler/src/main/java/versioneye/mojo/SpringPluginsMojo.java
+++ versioneye_maven_crawler/src/main/java/versioneye/mojo/SpringPluginsMojo.java
@@ -5,7 +5,7 @@ import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 
-@Mojo( name = "adobe", defaultPhase = LifecyclePhase.PROCESS_SOURCES )
+@Mojo( name = "springPlugins", defaultPhase = LifecyclePhase.PROCESS_SOURCES )
 public class SpringPluginsMojo extends HtmlMojo  {
 
     public void execute() throws MojoExecutionException, MojoFailureException {
