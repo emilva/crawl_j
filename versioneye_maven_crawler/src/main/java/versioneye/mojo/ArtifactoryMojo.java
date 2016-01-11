81a3e790abe90d447d3312ff8c61c1dfc6dba5fe
reiz <robert.reiz.81@gmail.com>
2016-01-11 16:44:00 +0100

    clean up.

--- versioneye_maven_crawler/src/main/java/versioneye/mojo/ArtifactoryMojo.java
+++ versioneye_maven_crawler/src/main/java/versioneye/mojo/ArtifactoryMojo.java
@@ -1,6 +1,5 @@
 package versioneye.mojo;
 
-
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
